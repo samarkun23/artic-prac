9fbf4bb5420b56851d3a2adeac48fed3c5389a2d
samarkun <samarkun4@gmail.com>
2026-03-20 13:24:17 +0530

    passing the custom data from the middleware

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,62 @@
-use actix_web::{App, Error, HttpResponse, HttpServer, Responder, body::MessageBody, dev::{ServiceRequest, ServiceResponse}, get, middleware::{Next, from_fn}, post, web::{self, Header, Json}};
- 
+use actix_web::{
+    App, Error, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder,
+    body::MessageBody,
+    dev::{ServiceRequest, ServiceResponse},
+    get,
+    middleware::{Next, from_fn},
+    post,
+    web::{self, Header, Json},
+};
 
 #[actix_web::main]
-async  fn main() {
-
-    HttpServer::new( move || { // move is used to move the ownership of person into the closure
+async fn main() {
+    HttpServer::new(move || {
+        // move is used to move the ownership of person into the closure
         App::new()
-            .route("/", web::get().to(|| async{
-                HttpResponse::Ok().body("hello world".to_string())
-            }))   
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("hello world".to_string()) }),
+            )
             .service(hello)
             .service(
                 web::scope("/world")
                     .route("", web::get().to(world))
-                    .wrap(from_fn(my_middleware))
+                    .wrap(from_fn(my_middleware)),
             )
             // .wrap(from_fn(my_middleware))
             .service(user)
             .default_service(web::to(not_found))
-            .service( // nest route & Scope 
+            .service(
+                // nest route & Scope
                 web::scope("/api")
-                .route("/nestroute", web::get().to(handler))
-                .route("/sec", web::get().to(sec))
+                    .route("/nestroute", web::get().to(handler))
+                    .route("/sec", web::get().to(sec)),
             )
-            
     })
-        .bind("0.0.0.0:3000")
-        .unwrap()
-        .run()
-        .await
-        .unwrap()
-
+    .bind("0.0.0.0:3000")
+    .unwrap()
+    .run()
+    .await
+    .unwrap()
 }
 
 #[get("/hello")]
-async  fn hello() -> impl Responder {
-    "Hello" 
+async fn hello(req: HttpRequest) -> impl Responder {
+    match req.extensions().get::<String>() {
+        Some(msg) => HttpResponse::Ok().body(msg.to_string()),
+        None => HttpResponse::Ok().body("No data"),
+    }
 }
 
 // #[get("/world")]
-async fn world() -> impl Responder {
-    String::from("world")
+async fn world(req: HttpRequest) -> impl Responder {
+    match req.extensions().get::<Person>() {
+        Some(person) => {
+            let msg = format!("person name {} and age is {}", person.name, person.age );
+            HttpResponse::Ok().body(msg)
+        },
+        None => HttpResponse::Ok().body("No data"),
+    }
 }
 
 #[get("/user")]
@@ -48,21 +64,31 @@ async fn user() -> impl Responder {
     HttpResponse::InternalServerError().body("user") // here so many error are avilable 
 }
 
-async fn not_found() -> impl Responder{
+async fn not_found() -> impl Responder {
     HttpResponse::NotFound().body("Not found")
 }
 
-async fn handler() -> impl Responder{
+async fn handler() -> impl Responder {
     HttpResponse::Ok().body("nestedRoute")
 }
 
-async fn sec() -> impl Responder{
+async fn sec() -> impl Responder {
     HttpResponse::Ok().body("sec_nestedRoute")
 }
 
-// middleware 
-async fn my_middleware(req: ServiceRequest, next: Next<impl MessageBody>) -> Result<ServiceResponse<impl MessageBody>, Error>{
+// middleware
+async fn my_middleware(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
     println!("HELLO FROM MIDDLEWARE");
-    // next.call(req).await
-    Ok(req.into_response(HttpResponse::Unauthorized().body("Unauthorized")))
+    let person = Person{name: String::from("Sam"), age: 20};
+    req.extensions_mut()
+        .insert(person);
+    next.call(req).await
+}
+
+struct Person{
+    name: String,
+    age: i32
 }
